2eb4dc79516c9b08f99fdc9143b0d6eeba5cceeb
PARKmaker <tkdgus8903@gmail.com>
2023-11-02 19:28:08 +0900

    프로젝추가 이력서 수정

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,46 +1,60 @@
 // TODO Add a couple lines about each project
 const data = [
-	{
-		title: "쇼핑몰 Hotdeal view",
-		subtitle: "쇼핑몰의 Hotdeal을 빠르게 파악할 수 있는 웹 페이지",
-		image: "/images/projects/hotdeal.PNG",
-		link: "https://github.com/PARKmaker/Hotdeal",
-		date: "2022-10",
-		desc:
-			"다양한 쇼핑몰마다 있는 Hotdeal 아이템을 하나의 사이트에서 볼 수 있는 웹." +
-			"최소의 시간을 사용해 다양한 쇼핑몰의 hotdeal 을 볼 수 있게 함.",
-	},
-	{
-		title: "AI 챗봇을 활용한 온라인 강의 웹 사이트",
-		subtitle: "캡스톤 디자인",
-		image: "/images/projects/HBNU-chatbot.PNG",
-		link: "https://github.com/HBNU-SWUNIV/COME-CAPSTONE22-chatbotcapstone",
-		date: "2022-07",
-		desc:
-			"캡스톤 디자인 프로젝트 입니다. " +
-			"실시간 소통의 한계가 있는 온라인 강의 환경에서 챗봇을 통해 질의응답을 수행할 수 있는 웹 어플리케이션 입니다." +
-			" 풀스택 개발을 진행 했습니다.",
-	},
-	{
-		title: "WhatsappClone",
-		subtitle: "Next.js 를 이용한 메시지 웹 어플리케이션",
-		link: "https://github.com/PARKmaker/WhatsappClone",
-		image: "/images/projects/whatsappClone.PNG",
-		date: "2022",
-		desc:
-			"Next.js, 메시지 통신 학습을 위한 Whatsapp 클론 프로젝트."
-	},
-	{
-		title: "Hacker News API 를 활용한 게시판",
-		subtitle: "Hacker News AP와 TypeScript를 이용한 웹 어플리 케이션",
-		link: "https://github.com/PARKmaker/HackerNews",
-		image: "/images/projects/hackernews.PNG",
-		date: "2022-08",
-		desc:
-			"TypeScript, Tailwind Css, Handlebars | " +
-			"TypeScript 학습을 위한 ‘패스트캠퍼스 강의’를 통한 클론 프로젝트"
-	},
-	
+  {
+    title: "React와 API를 활용한 Todo List",
+    subtitle: "개인 프로젝트",
+    image: "/images/projects/todo.PNG",
+    link: "https://github.com/PARKmaker/wanted-pre-onboarding-frontend",
+    date: "2023-09",
+    desc: "react-router-dom, useCallback, useMemo, Custom Hook을 사용하고 AWS S3 + CloudFront 배포 학습을 위한 프로젝트",
+  },
+  {
+    title: "법률 언어 생성현 모델을 활용한 생활 법률 지원 서비스",
+    subtitle: "단체 프로젝트",
+    image: "/images/projects/aivle.PNG",
+    link: "https://github.com/PARKmaker/lawABLE",
+    date: "2023-07",
+    desc: "챗봇을 활용한 질의응답과 법률 문서 작성을 도와주는 사이트 입니다.",
+  },
+  {
+    title: "AI 챗봇을 활용한 온라인 강의 웹 사이트",
+    subtitle: "캡스톤 디자인",
+    image: "/images/projects/HBNU-chatbot.PNG",
+    link: "https://github.com/HBNU-SWUNIV/COME-CAPSTONE22-chatbotcapstone",
+    date: "2022-07",
+    desc:
+      "캡스톤 디자인 프로젝트 입니다. " +
+      "실시간 소통의 한계가 있는 온라인 강의 환경에서 챗봇을 통해 질의응답을 수행할 수 있는 웹 어플리케이션 입니다." +
+      " 풀스택 개발을 진행 했습니다.",
+  },
+  {
+    title: "쇼핑몰 Hotdeal view",
+    subtitle: "쇼핑몰의 Hotdeal을 빠르게 파악할 수 있는 웹 페이지",
+    image: "/images/projects/hotdeal.PNG",
+    link: "https://github.com/PARKmaker/Hotdeal",
+    date: "2022-10",
+    desc:
+      "다양한 쇼핑몰마다 있는 Hotdeal 아이템을 하나의 사이트에서 볼 수 있는 웹." +
+      "최소의 시간을 사용해 다양한 쇼핑몰의 hotdeal 을 볼 수 있게 함.",
+  },
+  {
+    title: "WhatsappClone",
+    subtitle: "Next.js 를 이용한 메시지 웹 어플리케이션",
+    link: "https://github.com/PARKmaker/WhatsappClone",
+    image: "/images/projects/whatsappClone.PNG",
+    date: "2022",
+    desc: "Next.js, 메시지 통신 학습을 위한 Whatsapp 클론 프로젝트.",
+  },
+  {
+    title: "Hacker News API 를 활용한 게시판",
+    subtitle: "Hacker News AP와 TypeScript를 이용한 웹 어플리 케이션",
+    link: "https://github.com/PARKmaker/HackerNews",
+    image: "/images/projects/hackernews.PNG",
+    date: "2022-08",
+    desc:
+      "TypeScript, Tailwind Css, Handlebars | " +
+      "TypeScript 학습을 위한 ‘패스트캠퍼스 강의’를 통한 클론 프로젝트",
+  },
 ];
 
 export default data;
